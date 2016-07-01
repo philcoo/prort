e1ee6696e41b8c0c30825dcba92b9f347ee84f38
Philco Michel <phil9231@gmail.com>
2016-07-01 15:27:41 -0400

    edit html

--- server.rb
+++ server.rb
@@ -1,11 +1,18 @@
 require 'sinatra'
 
-get'/' do 
-	"hello world"
+get '/' do 
+	send_file File.join(settings.public_folder,'home.html')
+end
+ 
 
-	"this me "
+
+get'/education' do 
+	
+
+
 end
 
-get '/wyncode' do
-  "Hello wyncode"
+get '/about_me' do
+  # open and read about_me.html
 end
+
