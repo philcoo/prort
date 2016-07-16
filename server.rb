a7a04d63db02c6f086f9c8ab26b4f082289cca1b
Philco Michel <phil9231@gmail.com>
2016-07-15 20:40:19 -0400

    porfolio

--- server.rb
+++ server.rb
@@ -1,18 +1,6 @@
 require 'sinatra'
 
 get '/' do 
-	send_file File.join(settings.public_folder,'home.html')
+	send_file File.join(settings.public_folder,'about' ,'home.html')
 end
  
-
-
-get'/education' do 
-	
-
-
-end
-
-get '/about_me' do
-  # open and read about_me.html
-end
-
